4c755cb9d96cd53f7f83aaf62ed575a33896bedf
kenichi_shintome <tome119@gmail.com>
2020-06-13 16:47:59 +0900

    アナライザーパッケージを追加

--- AsyncTest/Program.cs
+++ AsyncTest/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace AsyncTest
 {
     class Program
     {
+        static async Task AsyncMethod()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+            Console.WriteLine("Done!");
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("start AsyncMethod");
+            var task = AsyncMethod();
+            task.Wait();
+            Console.WriteLine("end AsyncMethod");
         }
     }
 }
