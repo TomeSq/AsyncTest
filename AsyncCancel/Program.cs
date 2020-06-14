bcfeedbaa11a9279e16d49f3e30db48e93481f0b
kenichi_shintome <tome119@gmail.com>
2020-06-14 15:39:21 +0900

    タスクキャンセルの仕様を追記

--- AsyncCancel/Program.cs
+++ AsyncCancel/Program.cs
@@ -8,21 +8,38 @@ namespace AsyncTest
     {
         static async Task Main(string[] args)
         {
-            var task = HeavyMethod1();
 
-            HeavyMethod2();
+            using (var cts = new CancellationTokenSource())
+            {
+                try
+                {
 
-            Console.WriteLine(task.Result);
+                    cts.CancelAfter(1000);
+                    var task = HeavyMethod1(cts.Token);
+                    cts.Cancel();
 
+                    HeavyMethod2();
+
+                    await task;
+                }
+                catch (OperationCanceledException ex)
+                {
+                    Console.WriteLine($"タスクがキャンセルされました。{ex.Message}");
+                }
+            }
             Console.ReadKey();
         }
 
-        static async Task<string> HeavyMethod1()
+        static async Task HeavyMethod1(CancellationToken ct)
         {
             Console.WriteLine("すごく重い処理その1(´・ω・｀)はじまり");
+            //awaitがついているのでDelayが終わるまで次の処理は実行されない
             await Task.Delay(5000).ConfigureAwait(false);
+
+            //自分でチェックしないとタスクキャンセルの例外を投げられない
+//            ct.ThrowIfCancellationRequested();
+
             Console.WriteLine("すごく重い処理その1(´・ω・｀)おわり");
-            return "hoge";
         }
 
         static void HeavyMethod2()
