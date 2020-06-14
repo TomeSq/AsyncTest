d50d68e91a4d0f2d55bb67db5982bae6c838247b
kenichi_shintome <tome119@gmail.com>
2020-06-14 14:44:57 +0900

    非同期キャンセルプロジェクトを追加

---
+++ AsyncCancel/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncTest
+{
+    class Program
+    {
+        static async Task AsyncMethod()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+            Console.WriteLine("Done!");
+        }
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("start AsyncMethod");
+
+            //これは非同期
+            //_ = AsyncMethod().ConfigureAwait(false);
+
+            //これは以降が同期
+            await AsyncMethod().ConfigureAwait(false);
+            Console.WriteLine("end AsyncMethod");
+
+            Console.ReadKey();
+        }
+    }
+}
