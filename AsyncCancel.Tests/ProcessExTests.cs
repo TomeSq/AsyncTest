1f75b30c76770789a910d2d68a0f94ea4c59e2fb
kenichi_shintome <kshintome@say-tech.co.jp>
2020-06-22 10:07:42 +0900

    テストプロジェクトの追加

---
+++ AsyncCancel.Tests/ProcessExTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AsyncCancel.Tests
+{
+    public class ProcessExTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.False(true, "ƒeƒXƒgŽŽŒ±");
+        }
+    }
+}
