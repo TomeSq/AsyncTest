9c2d8c94272385842a735b8a5b6bfeaee99137a8
kenichi_shintome <kshintome@say-tech.co.jp>
2020-06-22 10:39:03 +0900

    テストでプロセスキャンセルを追加

--- AsyncCancel.Tests/ProcessExTests.cs
+++ AsyncCancel.Tests/ProcessExTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics;
+using System.Threading;
 using Xunit;
 
 namespace AsyncCancel.Tests
@@ -6,9 +8,39 @@ namespace AsyncCancel.Tests
     public class ProcessExTests
     {
         [Fact]
-        public void Test1()
+        public async void Procesタイムアウトキャンセル()
         {
-            Assert.False(true, "テスト試験");
+            //Arrange
+            var startInfo = new ProcessStartInfo()
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardError = false,
+                RedirectStandardOutput = false,
+                RedirectStandardInput = false,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                Arguments = "127.0.0.1 -n 10",
+                FileName = "ping",
+            };
+
+            //Act & Assert
+            using (var cts = new CancellationTokenSource())
+            using (var process = new Process()
+            {
+                StartInfo = startInfo,
+            })
+            {
+                cts.CancelAfter(TimeSpan.FromSeconds(5));
+                process.Start();
+                try
+                {
+                    _ = await process.WaitForExitAsync(cts.Token).ConfigureAwait(false);
+                    Assert.True(false, "OperationCanceledExceptionの例外が投げられませんでした。");
+                }
+                catch(OperationCanceledException)
+                {
+                }
+            }
         }
     }
 }
